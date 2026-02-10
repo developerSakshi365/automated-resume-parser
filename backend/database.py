864bcd3a2f2bbfa3b7fc5f7466ce2a03e43fe7f0
developerSakshi365 <developersakshi365@gmail.com>
2026-02-11 00:02:11 +0530

    Use DATABASE_URL from environment variables

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# CHANGE password and db name
-DATABASE_URL = "postgresql://postgres:sakshi365@localhost:5432/resume_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()   # VERY IMPORTANT
+Base = declarative_base()
